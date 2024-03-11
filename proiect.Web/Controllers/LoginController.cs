2ae34a96ce222a1905c2ae889713f67d061a2ca3
Ana-Maria <amungureanu15@gmail.com>
2024-03-11 23:34:54 +0200

    Proiect

---
+++ proiect.Web/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proiect.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+               UserLogin login = new UserLogin();
+
+            return View(login);
+        }
+          [HttpPost]
+          [ValidateAntiForgeryToken]
+          public ActionResult Index(UserLogin login)
+          {
+               if (NodelState.IsValid)
+               {
+                    Debug.WriteLine("Credential:" = +login.Credential);
+                    Debug.WriteLine("Password:" = +login.Password);
+               }
+               return View();
+          }
+    }
+}
---
+++ proiect.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="proiect.Web.MvcApplication" Language="C#" %>
